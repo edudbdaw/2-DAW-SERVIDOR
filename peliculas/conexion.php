4db0f035028bd7995d5caec17808a8b777300d63
Eduardo Duran <edudbdaw@gmail.com>
2025-09-30 16:19:23 +0100

    conexionEchaProyectoPeliculas

---
+++ peliculas/conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = 'localhost';
+$db_name = 'mi_proyecto_peliculas';
+$username = 'root';
+$passwd = '';
+
+try {
+    $conn = new PDO("mysql:host=$servername;$db_name" , $username , $passwd);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Conexion correcta";
+} catch (PDOException $e) {
+    die($e->getMessage());
+    
+}
---
+++ peliculas/registroform.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="registro.php" method="post">
+        <label for=""></label>
+        <label for="nombre">Nombre</label><input type="text" name="nombre" id="nombre">
+        <br>
+        <label for="apellido">Apellido</label><input type="text" name="apellido" id="apellido">
+        <br>
+        <label for="contraseña1">Contraseña</label><input type="password" name="contraseña" id="contraseña1">
+        <br>
+        <label for="contraseña1">Contraseña</label><input type="password" name="contraseña2" id="contraseña2">
+        <br>
+        <input type="submit" name="enviar" id="enviar">
+    </form>
+</body>
+</html>
