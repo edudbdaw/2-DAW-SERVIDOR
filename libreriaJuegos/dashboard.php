92658c7bee3df23a8ed6f262936143f389546abd
EduClase <edudbdawclase@gmail.com>
2025-10-21 20:05:09 +0200

    Pequeños cambios dashboard

--- libreriaJuegos/dashboard.php
+++ libreriaJuegos/dashboard.php
@@ -1,5 +1,20 @@
 <?php
     session_start();
+    require 'connbbdd.php';
+    if (!isset($_SESSION['user_id'])) {
+        header('Location:form_login.php');
+    }
+    try {
+            $stmt = $conn ->prepare("SELECT * from juegos where user_id = :user_id");
+            $stmt -> bindParam(':user_id' , $_SESSION['user_id']);
+            $stmt -> execute();
+
+            $juego = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            $errores [] = 'Error al recoger los datos de los juegos';
+            $_SESSION['errores'] = $errores;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,5 +26,14 @@
 </head>
 <body>
     <h1>Bienvenido <?php echo $_SESSION['username']?></h1>
+    <?php
+        if (isset($_SESSION['errores']) && !empty($_SESSION['errores'])) {
+            echo "<div>";
+            foreach($_SESSION['errores'] as $error) {
+                echo $error;
+            }
+            echo "</div>";
+        }
+    ?>
 </body>
 </html>
